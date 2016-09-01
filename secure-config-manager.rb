9d7ade6dea3c53c5ba536861e0261cd6b97bfc77
Alexey Savelev <savelichalex93@gmail.com>
2016-09-01 21:42:05 +0300

    Update file path in brew formula

--- secure-config-manager.rb
+++ secure-config-manager.rb
@@ -1,7 +1,7 @@
 class SecureConfigManager < Formula
   desc "Create secure config for swift project easeily"
   homepage "https://github.com/savelichalex/secure-config-manager"
-  url "https://github.com/savelichalex/secure-config-manager/archive/v0.1.0.tar.gz"
+  url "https://github.com/savelichalex/secure-config-manager/archive/v0.1.1.tar.gz"
   version "0.1.0"
   sha256 "49f384bb2de51663915c2b55131b7034c898aca8e7f0c5cdbf400c4a060b5ba8"
 
