bfca565730e45d290bf627465809d1427eed437e
Alexey <savelichalex93@gmail.com>
2016-09-01 21:46:05 +0300

    Update version in formula

--- secure-config-manager.rb
+++ secure-config-manager.rb
@@ -2,7 +2,7 @@ class SecureConfigManager < Formula
   desc "Create secure config for swift project easeily"
   homepage "https://github.com/savelichalex/secure-config-manager"
   url "https://github.com/savelichalex/secure-config-manager/archive/v0.1.1.tar.gz"
-  version "0.1.0"
+  version "0.1.1"
   sha256 "21833f8af47bb87aef94939f6c781c5c84d85093caa8f9fa22bfe3814d621cfa"
 
   def install
