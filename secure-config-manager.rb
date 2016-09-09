ed684a34ee72f711d7b469eec577b2933b17f820
Alexey <savelichalex93@gmail.com>
2016-09-09 19:01:20 +0300

    Update secure-config-manager.rb

--- secure-config-manager.rb
+++ secure-config-manager.rb
@@ -1,7 +1,7 @@
 class SecureConfigManager < Formula
   desc "Create secure config for swift project easeily"
   homepage "https://github.com/savelichalex/secure-config-manager"
-  url "https://github.com/savelichalex/secure-config-manager/archive/v0.2.0.tar.gz"
+  url "https://github.com/savelichalex/secure-config-manager/archive/0.2.0.tar.gz"
   version "0.2.0"
   sha256 "777523ce19a76aaa004ec2958a2ad75afe28e2dd2b955552c7264cd22270bb5c"
 
