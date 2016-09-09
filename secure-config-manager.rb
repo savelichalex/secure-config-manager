55b3ec8576e0a0b66a51e96e311f53a80bcecd6f
Alexey <savelichalex93@gmail.com>
2016-09-09 19:05:41 +0300

    Update secure-config-manager.rb

--- secure-config-manager.rb
+++ secure-config-manager.rb
@@ -3,7 +3,7 @@ class SecureConfigManager < Formula
   homepage "https://github.com/savelichalex/secure-config-manager"
   url "https://github.com/savelichalex/secure-config-manager/archive/0.2.0.tar.gz"
   version "0.2.0"
-  sha256 "777523ce19a76aaa004ec2958a2ad75afe28e2dd2b955552c7264cd22270bb5c"
+  sha256 "e4c27f1b6110531dad127f28e3d9425ff531fe6702024549ce352c2e9e1812b7"
 
   def install
     bin.install "scm"
